635d4940a081f4101ad48617563138afc69c15be
League Student <league@iMac-26.attlocal.net>
2019-07-08 16:31:02 -0700

    Update prime_or_nut.java
    i did simple calculator

--- src/prime_or_nut.java
+++ src/prime_or_nut.java
@@ -1,9 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class prime_or_nut {
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 99999999; i++) {
-
-		}
-
+		String number = JOptionPane.showInputDialog("pick a number");
+		
+int prime = Integer.parseInt(number);
+		
+for (int i = prime; i < args.length; i++) {
+	if(prime % 2
+}
 	}
 }
